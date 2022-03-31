501f67ad5a02faddbf1842fb394d969f7dbcb126
nquangphu <nguyenphu14302@gmail.com>
2022-03-31 08:41:41 +0700

    update procedure grant_select_to_user

--- sql/grant_privileges.sql
+++ sql/grant_privileges.sql
@@ -65,13 +65,13 @@ CREATE OR REPLACE PROCEDURE grant_delete_to_user (
     username IN NVARCHAR2,
     table_name IN NVARCHAR2,
     have_WGO IN INTEGER)
-IS str VARCHAR2(100);
+IS str VARCHAR2(1000);
 BEGIN 
     str := 'GRANT DELETE ON ' || table_name || ' TO ' || username;
     IF have_WGO = 1 THEN
         str := CONCAT(str,' WITH GRANT OPTION');
     END IF;
-    
+    DBMS_OUTPUT.PUT_LINE(str);
     EXECUTE IMMEDIATE (str);
         
 	COMMIT;
@@ -80,41 +80,43 @@ END grant_delete_to_user;
 -- Cap quyen select
 -- have_WGO = 1 => WITH GRANT OPTION
 -- have_WGO = 0 => NO WITH GRANT OPTION
--- cmd_line_create_view: lenh tao view. Vi du: 'CREATE TABLE OR REPLACE VIEW view_name AS SELECT * FROM table_name'
 CREATE OR REPLACE PROCEDURE grant_select_to_user (
     username IN NVARCHAR2,
-    view_name IN NVARCHAR2,
-    cmd_line_create_view IN NVARCHAR2,
-    have_WGO IN INTEGER)
-IS str VARCHAR2(100);
+    table_name IN NVARCHAR2,
+    lst_column IN VARCHAR2,
+    have_WGO IN INTEGER,
+    view_name IN VARCHAR2)
+IS str VARCHAR2(1000);
 BEGIN
-    EXECUTE IMMEDIATE (cmd_line_create_view);
-    
+    str := 'CREATE OR REPLACE VIEW ' || view_name || ' AS SELECT ' || lst_column || ' FROM ' || table_name;
+    DBMS_OUTPUT.PUT_LINE(str);
+    EXECUTE IMMEDIATE (str);
+    str := '';
     str := 'GRANT SELECT ON ' || view_name || ' TO ' || username;
     IF have_WGO = 1 THEN
         str := CONCAT(str,' WITH GRANT OPTION');
     END IF;
-    
+    DBMS_OUTPUT.PUT_LINE(str);
     EXECUTE IMMEDIATE (str);
         
 	COMMIT;
 END grant_select_to_user;
 /
-
+-- EXEC grant_select_to_user('C##TEST', 'table_test', 'username, pwd', 1, 'view_USER_PWD_TEST');
 -------------------- REVOKE --------------------
 /*==============================================*/
 CREATE OR REPLACE PROCEDURE revoke_privileges_from_user (
     username IN NVARCHAR2,
     table_name IN NVARCHAR2,
     lst_privileges IN NVARCHAR2)
-IS str VARCHAR2(100);
+IS str VARCHAR2(1000);
 BEGIN
     str := 'REVOKE ' || lst_privileges || ' ON ' || table_name || ' FROM ' || username;
     DBMS_OUTPUT.PUT_LINE(str);
     EXECUTE IMMEDIATE (str);
 	COMMIT;
 END revoke_privileges_from_user;
-
+/
 /*
 CREATE TABLE table_test (
     id_user NUMBER PRIMARY KEY,
@@ -129,8 +131,7 @@ GRANT create session TO C##TEST;
 GRANT RESOURCE, CONNECT TO C##TEST;
 GRANT SELECT (id_user) ON table_test TO C##TEST;
 select * from table_test;
-EXEC grant_insert_to_user('C##TEST', 'table_test', 1);
 EXEC grant_delete_to_user('C##TEST', 'table_test', 1);
-EXEC grant_select_to_user('C##TEST', 'view_name', 'CREATE OR REPLACE VIEW view_name AS SELECT username FROM table_test WHERE id_user=1', 0);
+
 EXEC grant_update_to_user('C##TEST', 'view_name', 'CREATE OR REPLACE VIEW view_name AS SELECT username FROM table_test WHERE id_user=1', 0);
 */
